fc5583a29fa59411c1f5734d14e3ddb02121ab29
Diego Capre <dcapre@live.com>
2019-03-28 16:06:11 -0400

    Chart and Table
    Using dummy data.
    
    Added chart to view sales of 10 reports, and a button to switch between "Empanados" and "Empanadillas".
    
    Added Table to hold chart values and a button to test if the fields inside the chart can change if named properly.

---
+++ src/profile/map.js
@@ -0,0 +1,7 @@
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 18.209527, lng: -67.139727},
+          zoom: 15
+        });
+      }
