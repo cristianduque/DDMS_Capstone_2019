53dc94eef2628aaae9aa5f226db6f32251e8d252
janrobles <jan.robles@upr.edu>
2019-03-30 18:25:38 -0400

    Fields added(pics, credit units, etc.)

--- src/components/DemoForm.vue
+++ src/components/DemoForm.vue
@@ -1,19 +1,18 @@
 <template>
 <div>
-  <h1> Hello! Mi chiamo Jan from Demo Form </h1>
 
-
-  <h2>Demo Form</h2>
+  <h2>Nombre Tienda:___________   Pueblo:_____________ </h2>
+  <h3>Fecha:________________</h3>
 
 
 <form action="/action_page.php">
   <p> Seleccione producto a demostrar: </p>
 
   <select name="products">
-    <option value="emp-pollo">emp-pollo</option>
-    <option value="emp-pavo">emp-pavo</option>
-    <option value="emp-bistec">Emp-bistec</option>
-    <option value="longaniza">Longaniza</option>
+    <option value="Emp-pollo">Emp-pollo</option>
+    <option value="Emp-pavo">Emp-pavo</option>
+    <option value="Emp-bistec">Emp-bistec</option>
+    <option value="Longaniza">Longaniza</option>
   </select>
   <br><br>
   Precio Regular:
@@ -28,19 +27,36 @@
   <input name="inv_initial" type="number">
   <br><br>
 
+  Foto Inventario Inicial:
+  <input type="file" name="initial_pic" accept="image/*">
+  <br><br>
+
   Inventario Final:
   <input name="inv_final" type="number">
+  <br><br>
+
+  Foto Inventario Final:
+  <input type="file" name="final_pic" accept="image/*">
   <br><br>
 
   Total Unidades Vendidas:
   <output name="vendidas" type="number"></output>
   <br><br>
 
+  Hubo unidades de crédito?
+  <form>
+
+    <input type="radio" name="credit" value="Yes"> Yes
+    <input type="radio" name="credit" value="No"> No<br>
+
+  </form>
+  <br>
+
   Total Unidades Creditos:
   <input name="creditos" type="number">
   <br><br>
  <button type="anadirProducto">Add another product</button>
- </br></br></br>
+ <br><br><br>
   <input type="submit">
 </form>
 
